143e37a49c8bc3c789d80340a627766ea4128d04
LA1975 <you@example.com>
2022-07-03 13:45:55 +1200

    file name changes and chane of fonts

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ while(element.hasChildNodes()) element.removeChild(element.firstChild);
 
 for(let i = 0; i < letters.length; i++) {
   let letter = document.createElement('span'),
-  style = 'opacity ' + (Math.random() *5 +1 ) + 's linear';
+  style = 'opacity ' + (Math.random() *5 +1 ) + 's ease-in-out';
   letter.appendChild(document.createTextNode(letters[i]));
   letter.style.WebKitTransition = letter.style.MozTransition = 
   letter.style.MSTransition = letter.style.OTransition = letter.style.transition = style;
