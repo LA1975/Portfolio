9c9b3a8744988256ea1aa1fdb5b1fc2fd6f4c7b5
LA1975 <you@example.com>
2022-07-10 12:46:09 +1200

    reorganised folders.  Removed side navigation icons and replace with social media buttons

---
+++ src/app.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function () {
+
+let stringName = document.getElementById("name").innerHTML;
+let elementName = this.document.getElementById("name");
+randomLetterFadeIn(stringName, elementName);
+
+let stringDescipt = this.document.getElementById("descript").innerHTML;
+let elementDescript = this.document.getElementById("descript");
+randomLetterFadeIn(stringDescipt,elementDescript);
+
+function randomLetterFadeIn(string, element){
+
+let  letters = string.split('');
+while(element.hasChildNodes()) element.removeChild(element.firstChild);
+
+for(let i = 0; i < letters.length; i++) {
+  let letter = document.createElement('span'),
+  style = 'opacity ' + (Math.random() *5 +1 ) + 's ease-in-out';
+  letter.appendChild(document.createTextNode(letters[i]));
+  letter.style.WebKitTransition = letter.style.MozTransition = 
+  letter.style.MSTransition = letter.style.OTransition = letter.style.transition = style;
+  letter.style.opacity = 0;
+  element.appendChild(letter);
+}
+
+setTimeout(function() {
+  for(let i = 0; i < element.childNodes.length; i++) {
+    element.childNodes[i].style.opacity = 1;
+  }
+}, 0);
+}
+
+
+
+});
