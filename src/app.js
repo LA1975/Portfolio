158743b70f6caf8bd29a16d04ae481d46b84c6b3
LA1975 <you@example.com>
2022-07-11 12:03:37 +1200

    responsiveness finished.  Next want to change static hamburger icon button into an animated

--- src/app.js
+++ src/app.js
@@ -30,6 +30,13 @@ setTimeout(function() {
 }, 0);
 }
 
+const hamburgerBtn = document.getElementById('hamburger');
+const navList = document.getElementById('navList');
+ 
 
+function toggleButton(){
+  navList.classList.toggle('show');
+ }
 
+hamburgerBtn.addEventListener('click', toggleButton);
 });
