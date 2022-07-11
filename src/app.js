9c390a07042509d4b6cb380cb57bcdaad37f50e0
LA1975 <you@example.com>
2022-07-11 13:40:56 +1200

    Animated hamburger getting a small jump from lines to cross position due to flex justify end.

--- src/app.js
+++ src/app.js
@@ -32,10 +32,32 @@ setTimeout(function() {
 
 const hamburgerBtn = document.getElementById('hamburger');
 const navList = document.getElementById('navList');
- 
+const firstBar = document.getElementById('bar-first');
+const secondBar = document.getElementById('bar-second');
+const thirdBar = document.getElementById('bar-third');
+let menuOn=false;
 
 function toggleButton(){
   navList.classList.toggle('show');
+  
+  
+  if(menuOn){
+    firstBar.style.transform = 'rotate(0deg)';
+    firstBar.style.top = '0px';
+    secondBar.style.opacity =1;
+    thirdBar.style.transform = 'rotate(0deg)';
+    thirdBar.style.bottom = '0px';
+    menuOn=false;
+    
+  }else{
+
+    firstBar.style.transform = 'rotate(45deg)';
+    firstBar.style.top = '8px';
+    secondBar.style.opacity =0;
+    thirdBar.style.transform = 'rotate(-45deg)';
+    thirdBar.style.bottom = '8px';
+    menuOn=true;
+  }
  }
 
 hamburgerBtn.addEventListener('click', toggleButton);
