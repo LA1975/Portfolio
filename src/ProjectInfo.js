d894231f2cf17ec5d02041f24e8d17a9b68ced07
LA1975 <you@example.com>
2023-07-17 15:48:08 +1200

    added now project

--- src/ProjectInfo.js
+++ src/ProjectInfo.js
@@ -1,9 +1,20 @@
 const project = [
     {
+        fileName: "Notes Mad screenshot.png",
+        title: "Notes Mad Website",
+        description: "Notes Mad was inspired by an online tutorial for a simple front-end app.  I made it into a full stack app with user login and admin facilities.  HTML, CSS, Javascript, node.js, express, handlebars, SQLite",
+        gitHubLink:"https://github.com/LA1975/Notes-Mad.git"
+    },
+    {
+        fileName: "IntroInstructions.png",
+        title: "Cows and Bulls Game",
+        description: "Cows and Bulls game developed for an assignment.  Java",
+        gitHubLink:"https://github.com/LA1975/Cows-and-Bulls-Game-Reflection.git"
+    },
+    {
         fileName: "Portfolio Header.png",
         title: "Portfolio Website",
         description: "This was a project to develop a personal portfolio site.  HTML, CSS, Javascript",
         gitHubLink:"https://github.com/LA1975/Portfolio.git"
     },
-
 ]
